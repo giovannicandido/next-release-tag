bfc2d2ff0d7d6a4759acf546348ddec10ed084a6
Giovanni Silva <giovanni@kugelbit.com>
2023-06-25 20:31:53 -0300

    try fix latestTags query

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -8,36 +8,39 @@ export const fetchLatestReleaseTag = async () => {
     const octokit = getOctokit(githubToken);
     const { owner, repo } = context.repo;
     if(useTagInsteadOfRelease) {
-      const { repository } = await octokit.graphql<{repository: any}>(
+      const { latestTags } = await octokit.graphql<{latestTags: any}>(
         `
-        query repository($owner: String!, $repo: String!) {
-          refs(refPrefix: "refs/tags/", first: 1, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {
-            edges {
-              node {
-                name
-                target {
-                  oid
-                  ... on Tag {
-                    message
-                    commitUrl
-                    tagger {
-                      name
-                      email
-                      date
+          query latestTags($owner: String!, $repo: String!) {
+            repository($owner: String!, $repo: String!) {
+              refs(refPrefix: "refs/tags/", first: 1, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {
+                edges {
+                  node {
+                    name
+                    target {
+                      oid
+                      ... on Tag {
+                        message
+                        commitUrl
+                        tagger {
+                          name
+                          email
+                          date
+                        }
+                      }
                     }
                   }
                 }
               }
             }
           }
-        }
+    
         `,
         {
           owner: owner,
           repo: repo,
         }
       );
-      return repository.name;
+      return latestTags.name;
     } else {
       const response = await octokit.rest.repos.getLatestRelease({
         owner,
